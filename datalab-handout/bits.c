2a47d5528654c48c88d7ef0d3c2aa4eef2f253b3
anyaliang <anyaliangww@gmail.com>
2017-03-18 19:57:32 +0800

    edit datalab

--- datalab-handout/bits.c
+++ datalab-handout/bits.c
@@ -199,25 +199,6 @@ int bitCount(int x) {
     return (s + (s >> 8)) & 0x3f;
 
 }
-/*
-    int count; 
-    int tmpMask1 = (0x55)|(0x55<<8); 
-    int mask1 = (tmpMask1)|(tmpMask1<<16); 
-    int tmpMask2 = (0x33)|(0x33<<8); 
-    int mask2 = (tmpMask2)|(tmpMask2<<16); 
-    int tmpMask3 = (0x0f)|(0x0f<<8); 
-    int mask3 = (tmpMask3)|(tmpMask3<<16); 
-    int mask4 = (0xff)|(0xff<<16); 
-    int mask5 = (0xff)|(0xff<<8); 
-    count = (x&mask1)+((x>>1)&mask1); 
-    count = (count&mask2)+((count>>2)&mask2); 
-    count = (count + (count >> 4)) & mask3; 
-    count = (count + (count >> 8)) & mask4; 
-    count = (count + (count >> 16)) & mask5; 
-    return count;
-}
-*/
-
     
 /*
   int mask1 = 0x11111111;
