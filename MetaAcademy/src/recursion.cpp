6d73bc2f7e32bdb73231cb0442ee8cc280d143e5
anyaliang <anyaliangww@gmail.com>
2017-03-25 14:14:50 +0800

    Update MeatAcademy

--- MetaAcademy/src/recursion.cpp
+++ MetaAcademy/src/recursion.cpp
@@ -11,24 +11,37 @@
 #include "gwindow.h"
 #include "gobjects.h"
 #include "tokenscanner.h"
+
 using namespace std;
 
+/*
+ * 最大公分母 Greatest Common Denominator
+ */
 int gcd(int a, int b) {
-    // your code here
-    cout << "[recursion gcd called]" << endl;
-    return 0;
+    if(b==0) {
+        cout << "gcd(" << a << ", " << b << ") = " << a << endl;
+        return a;
+    }
+    else {
+        cout << "gcd(" << a << ", " << b << ") = " << "gcd(" << b << ", " << a%b << ")" << endl;
+        return gcd(b, a%b);
+    }
 }
 
+/*
+ * Serpinskii 分形
+ */
 void serpinskii(GWindow &w, int leftX, int leftY, int size, int order) {
-    // your code here
     cout << "[recursion serpinskii called]" << endl;
 }
+
 
 int floodFill(GBufferedImage& image, int x, int y, int newColor) {
     // your code here
     cout << "[recursion flood fill called]" << endl;
     return 0;
 }
+
 
 void personalCurriculum(Map<string, Vector<string>> & prereqMap,string goal) {
     // your code here
