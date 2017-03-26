329cb9718a8540bcb387e6fc52cf534091c8db94
anyaliang <anyaliangww@gmail.com>
2017-03-26 12:21:14 +0800

    Update repo

--- Stanford-CS106B/MetaAcademy/src/recursion.cpp
+++ Stanford-CS106B/MetaAcademy/src/recursion.cpp
@@ -11,7 +11,6 @@
 #include "gwindow.h"
 #include "gobjects.h"
 #include "tokenscanner.h"
-
 using namespace std;
 
 /*
@@ -30,18 +29,35 @@ int gcd(int a, int b) {
 
 /*
  * Serpinskii 分形
+ * write a func named () in meta.cpp
  */
+void DrawTri(GWindow &w, int leftX, int leftY, int nsize) {
+    GPoint lef;
+    GPoint rig;
+    GPoint top;
+    rig = w.drawPolarLine(leftX, leftY, nsize, -60);
+    lef = w.drawPolarLine(leftX, leftY, nsize, -120);
+    top = w.drawPolarLine(leftX, leftY, 0, 0);
+    w.drawLine(rig, lef);
+}
+
 void serpinskii(GWindow &w, int leftX, int leftY, int size, int order) {
+    DrawTri(w, leftX, leftY, size);
     cout << "[recursion serpinskii called]" << endl;
 }
 
-
 int floodFill(GBufferedImage& image, int x, int y, int newColor) {
-    // your code here
-    cout << "[recursion flood fill called]" << endl;
+    //if(x>image.getWidth()||y>image.getHeight());
+    //else {
+        image.setRGB(x, y, newColor);
+
+      floodFill(image, x+1, y, newColor);
+      floodFill(image, x, y+1, newColor);
+      floodFill(image, x-1, y, newColor);
+      floodFill(image, x, y-1, newColor);
+  //  }
     return 0;
 }
-
 
 void personalCurriculum(Map<string, Vector<string>> & prereqMap,string goal) {
     // your code here
