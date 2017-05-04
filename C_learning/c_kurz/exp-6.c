ece62c9343d32591a71916a45eb5789eebd3f41b
anyaliang <anyaliangww@gmail.com>
2017-05-04 20:01:18 +0800

    Updated c_kurz/exp-6

--- C_learning/c_kurz/exp-6.c
+++ C_learning/c_kurz/exp-6.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 
+void bubble_sort(int arr[], int total);
+void selection_sort(int a[], int total);
+void merge(int c[], int *nc, int a[], int na, int b[], int nb);
+
 int main(void){
     int arr_1[10] = {0};
     int arr_2[10] = {0};
     int tmp1 = 0, tmp2 = 0;
     int max = 0;
+    int sorted[100] = {0};
     int k = 0;
+    int *nc = 0;
     
     printf("enter first array: \n");
     for(int i=0; i<10; i++){
@@ -16,46 +22,66 @@ int main(void){
         scanf("%d", &arr_2[i]);
     }
     
-    for(int m=1; m<10; m++){
-        for(int n=0; n<10-m; n++){
-            if(arr_1[n]<arr_1[n+1]){
-                tmp1 = arr_1[n];
-                arr_1[n] = arr_1[n+1];
-                arr_1[n+1] = tmp1;
-            }
-        }
-    } //bubble sort for arr_1
+    bubble_sort(arr_1, 10);
+    selection_sort(arr_2, 10);
+    merge(sorted, nc, arr_1, 10, arr_2, 10);
 
-    for(int s=0; s<9; s++){
-        max_index = s;
-        for(int l=s+1; s<10; l++){
-            if(arr_2[l]>arr_2[max_index]) {
-                max_index = l;
-                tmp2 = arr_2[s];
-                arr_2[s] = arr_2[max_index];
-                arr_2[max_index] = tmp2;
+    return 0;
+}
+
+void bubble_sort(int arr[], int total){
+    int tmp = 0;
+    for(int i=0; i<total-1; i++){
+        for(int j=0; j<total-1-i; j++){
+            if(arr[j+1]<arr[j]){
+                tmp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = tmp;
             }
         }
     }
+}
 
-    
-    for(int i=0; i<9; i++){
-        k = i;
-        for(int j=i+1; j<10; i++){
-            if(arr_2[k]<arr_2[j]){
-                max = j;
-                tmp2 = arr_2[i];
-                arr_2[i] = arr_2[k];
-                arr_2[k] = tmp2;
-            }
+void selection_sort(int a[], int total){
+    int max_index = 0, tmp = 0;
+    for(int i=0; i<total-1; i++){
+        max_index = i;
+        for(int j=i+1; j<total; j++){
+            if(a[max_index]>a[j]) max_index = j;
+
+            tmp = a[max_index];
+            a[max_index] = a[i];
+            a[i] = tmp; //when ends a line, exchage between a[max] and **a[i]**
         }
-    } //为啥不行啊啊啊啊啊啊啊啊啊啊我的选择排序有毒啊啊啊啊啊
+    }
+}
 
-    int sort[20] = {0};
-    int count1 = 0, count2 = 0;
-    (arr_1[count1]>arr_1[count2])? sort[1] = arr_1[count1] count1++; : arr_2[count2]count2++;
-    if(count1==9) put another allin;
-    if(count2==9) put another allin;
+/*
+int merge(int sorted[],  int a[], int ano, int b[], int bno){
+    int a_count = 0, b_count = 0 , c_count = 0;
+    while(a_count<ano&& b_count<bno) 
+        a[a_count]>b[b_count]? sorted[c_count++]=a[a_count++]: sorted[c_count++]=b[b_count++];
 
-    return 0;
+    return c_count;
+}
+*/
+
+void merge(int c[], int *nc, int a[], int na, int b[], int nb){
+    int cursora, cursorb, cursorc;
+
+    cursora=cursorb=cursorc=0;
+
+    while((cursora<na)&&(cursorb<nb))
+        if (a[cursora]<=b[cursorb])
+            c[cursorc++]=a[cursora++];
+        else
+            c[cursorc++]=b[cursorb++];
+
+    while(cursora<na)
+        c[cursorc++]=a[cursora++];
+
+    while(cursorb<nb)
+        c[cursorc++]=b[cursorb++];
+
+    *nc = cursorc;
 }
