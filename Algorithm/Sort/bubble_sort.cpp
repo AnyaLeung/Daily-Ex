ac6ba0714811a345a2c882e012560534086de856
anyaliang <anyaliangww@gmail.com>
2017-05-04 19:34:25 +0800

    Updated sort

--- Algorithm/Sort/bubble_sort.cpp
+++ Algorithm/Sort/bubble_sort.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 
-for(int i=1; i<num; i++){
-    for(int j=0; j<num-1; j++){
-        if(arr[j]>arr[]) 
+using namespace std;
+
+void bubble_sort(int arr[], int total);
+
+int main(void){
+    int a[10] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    bubble_sort(a, 10);
+    for(int i=0; i<10; i++){
+        printf("%d", a[i]);
+    }
+    return 0;
+}
+
+void bubble_sort(int arr[], int total){
+    int tmp = 0;
+    for(int i=0; i<total-1; i++){
+        for(int j=0; j<total-1-i; j++){
+            if(arr[j+1]<arr[j]){
+                tmp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = tmp;
+            }
+        }
     }
 }
---
+++ Algorithm/Sort/selection_sort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+void selection_sort(int a[], int total);
+
+int main(void){
+    int a[10] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    selection_sort(a, 10);
+    for(int i=0; i<10; i++){
+        printf("%d", a[i]);
+    }
+    return 0;
+}
+
+void selection_sort(int a[], int total){
+    int max_index = 0, tmp = 0;
+    for(int i=0; i<total-1; i++){
+        max_index = i;
+        for(int j=i+1; j<total; j++){
+            if(a[max_index]>a[j]) max_index = j;
+
+            tmp = a[max_index];      
+            a[max_index] = a[i]; 
+            a[i] = tmp; //when ends a line, exchage between a[max] and **a[i]**
+        }    }
+}
+/*
+ * basic thought:
+ * 把整个数组过很多遍，第一遍找出最大的和第一个换，第二遍找第二大的和第二个换…
+ * 以此类推
+ *
+ * 第n趟结束的时候，把这一趟最大（小）的和第n个换
+ */
