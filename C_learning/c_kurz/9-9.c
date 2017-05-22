86c41e5dcb2afd50969cf9e9340bfb294f98a986
anyaliang <anyaliangww@gmail.com>
2017-05-22 16:47:09 +0800

    Updated 9-9.c

--- C_learning/c_kurz/9-9.c
+++ C_learning/c_kurz/9-9.c
@@ -11,51 +11,45 @@ int main(void){
     int res[1000] = {0};
     int lena = 0, lenb = 0;
     int i = 0, j = 0, k = 0;
-    int flag = 0;
-
+    int temp = 0;
+    
     printf("被减数： ");
     gets(a);
     printf("减数： ");
     gets(b);
-
+    
     lena = strlen(a);
     lenb = strlen(b);
-
+    
     Judge(lena, lenb, a, b);
-
-    for(; i<lena; i++){
-        res[lena-i-1] = (int)(a[lena-i-1]-'0') - (int)(b[lenb-i-1]-'0'); 
-        if(res[lena-i-1]>9) res[lena-i-1] -= 48;
+    
+    if(sign=='-'){
+        temp = lena;
+        lena = lenb;
+        lenb = temp;
     }
 
-    for(; j<lena; j++){
-        if(res[lena-j-1]<0){
-            res[lena-j-2] --;
-            res[lena-j-i] += 10;
+    for(i=0; i<lena; i++){
+        res[lena-i-1] = (int)(a[lena-i-1]-'0') - (int)(b[lenb-i-1]-'0');
+        if(res[lena-i-1]>10) res[lena-i-1] -= (int)'0';
+    }
+    
+    for(j=lena-1; j>=0; j--){
+        if(res[j]<0){
+            res[j-1] --;
+            res[j] += 10;
         }
     } //借位
-
+  
     if(sign=='+') ;
     else printf("-"); //打印符号
-
-   for(; k<lena; k++){
-       printf("%d", res[k]);
-   }
-
+    
+    for(k=0; k<lena; k++){
+        printf("%d", res[k]);
+    }
+    
     return 0;
 }
-/*
-void Judge(int lena, int lenb, char *a, char *b){ //判断减数和被减数的大小关系
-    char *tmp;
-    if(lena>=lenb&& strcmp(a,b)>=0) sign = '+';
-    else {
-        sign = '-';
-        strcpy(tmp, a);
-        strcpy(a, b);
-        strcpy(b, tmp);
-    } 
-}
-*/
 
 void Judge(int lena, int lenb, char *a, char *b){ //判断减数和被减数的大小关系
     char *tmp;
