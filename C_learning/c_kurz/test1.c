8fdc4e02c62e2ac4cf5e99d0f415efbd2030edff
anyaliang <anyaliangww@gmail.com>
2017-05-25 14:57:08 +0800

    Updated c_kurz

--- C_learning/c_kurz/test1.c
+++ C_learning/c_kurz/test1.c
@@ -1,18 +1,47 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct Stu{
+    int no;
+    char name[10];
+    char major[10];
+    int score;
+};
+
+struct Stu score[5], temp;
+void selection_sort(struct Stu *, int n);
 
 int main(void){
-    double sum = 1;
-    double value = 0;
-    float x = 0;
+    int i  = 1;
+    int n = 0;
+    FILE *fp;
 
-    scanf("%f", &x);
-    value = 1.0 / x; //
+    fp = fopen("info.txt", "r");
+    while(fscanf(fp, "%d %s %s %d", &score[n].no, score[n].name, score[n].major, &score[n].score)!=EOF)
+       n++;
 
-    while(value>0.000001){
-        sum += value;
-        value =  value * (1 / x);
+    selection_sort(score, 5);
+    for(n=0; n<5; n++){
+    printf("%d %s %s %d\n", score[n].no, score[n].name, score[n].major, score[n].score);
     }
 
-    printf("res: %lf", sum);
     return 0;
+}
+
+void selection_sort(struct Stu *, int n){
+    int i=0, j=0, k=0;
+    for(i=0; i<n-1; i++){
+        k = i;
+        for(j=i+1; j<n; j++){
+            if(score[j].score>score[k].score){
+                k = j;
+                if(k!=i){
+                    temp = score[i];
+                    score[i] = score[k];
+                    score[k] = temp;
+                }
+            }
+        }
+    }
 }
