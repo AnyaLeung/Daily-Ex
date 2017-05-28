3e590fd878426650da6c8c9761831abbba5fcc10
anyaliang <anyaliangww@gmail.com>
2017-05-28 22:04:04 +0800

    Updated c_exp

--- C_learning/c_kurz/exp-5.c
+++ C_learning/c_kurz/exp-5.c
@@ -1,32 +1,13 @@
 #include <stdio.h>
 
 int main(void){
-    int no = 0;
-    int i = 0;
-    int j = 0;
+    int n = 0, i = 0, j = 0, no3 = 0, tmp = 0;
     int sum = 0;
-    int no3 = 0;
-    int flag = 0;
-    int  *rem;
-    int k = 0;
 
-    scanf("%d", &no);
-    no3 = no * no * no;
+    scanf("%d", &n);
+    no3 = n * n * n;
+    for(j=i; j<i)
 
-    for(i=1; i<no&& flag==0; i+=2){
-        for(j=0; j<no; j++){
-            sum += i;
-            if(sum==no3) {
-                break;
-                flag = 1;
-            }
-        }
-    }
-
-    for(k=0; k<no; k++){
-        printf("%d", i);
-        i -= 2;
-    }
 
     return 0;
 }
