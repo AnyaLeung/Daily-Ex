daf49ebea670348a1b7f616f33fbf0cd202c98a3
anyaliang <anyaliangww@gmail.com>
2017-07-27 22:01:07 +0800

    update Sort

--- Algorithm/Sort/bubble_sort.cpp
+++ Algorithm/Sort/bubble_sort.cpp
@@ -5,8 +5,15 @@ using namespace std;
 void bubble_sort(int arr[], int total);
 
 int main(void){
-    int a[10] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    int a[10] = {0};
+    printf("before sort: ");
+    for(int j=0; j<10; j++){
+        a[j] = j + 1;
+        printf("%d", a[j]);
+    }
+
     bubble_sort(a, 10);
+    printf("\nafter sort:  ");
     for(int i=0; i<10; i++){
         printf("%d", a[i]);
     }
@@ -17,7 +24,7 @@ void bubble_sort(int arr[], int total){
     int tmp = 0;
     for(int i=0; i<total-1; i++){
         for(int j=0; j<total-1-i; j++){
-            if(arr[j+1]<arr[j]){
+            if(arr[j+1]>arr[j]){
                 tmp = arr[j+1];
                 arr[j+1] = arr[j];
                 arr[j] = tmp;
@@ -25,3 +32,4 @@ void bubble_sort(int arr[], int total){
         }
     }
 }
+//递减，并逐渐把小的固定最后
