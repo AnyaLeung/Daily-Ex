e997389084f34d26a1a8e9e2b101fc3035fc2fc6
anyaliang <anyaliangww@gmail.com>
2017-07-29 20:30:37 +0800

    updated

--- LCE/1.c
+++ LCE/1.c
@@ -1,16 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 int* twoSum(int* nums, int numsSize, int target) {
     int i = 0, j = 0;
     int* res;
-    bool flag = 1;
-    for(i=0; i<numsSize&& flag; i++){
-        for(j=0; j<numsSize&& flag; j++){
-            if(j!=i&& (nums[i]+nums[j]==target)){
-                flag = 0;
+    res = (int*)malloc(2 * sizeof(int));
+    for(i=0; i<numsSize-1; i++){
+        for(j=i+1; j<numsSize; j++){
+            if(nums[i]+nums[j]==target){
+                res[0] = i;
+                res[1] = j;
+                return res; 
+                //为啥这句不写就不对啊。。
             }
         }
     }
-    res = (int *)malloc(sizeof(int) * 2);
-    res[0] = i - 1;
-    res[1] = j - 1;
     return res;
+}
+
+int main(void){
+    int nums[4] = {1, 2, 3, 4};
+    int* res;
+    res = twoSum(nums, 4, 7);
+    for(int i=0; i<2; i++){
+        printf("%d ", res[i]);
+    }
+    return 0;
 }
