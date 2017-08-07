0e9376141be9aa03677c698529dd99e5d1ef5af5
anyaliang <anyaliangww@gmail.com>
2017-08-07 11:28:03 +0800

    update algorithm

--- Algorithm/Sort/selection_sort.py
+++ Algorithm/Sort/selection_sort.py
@@ -1,10 +1,18 @@
-anfang = 0
-
-def suchen_grosst(list, anfang=0):
-    gross = list[m]
-    for i in len(list)-1:
+def suchen_grosst(list):
+    g_index = 0
+    gross = list[0]
+    for i in range(len(list)):
         if list[i] > gross:
-            list[i], gross = gross, list[i]
+            gross = list[i]
+            g_index = i
+    return g_index
 
 def selection_sort(list):
-    
+    res = []
+    for i in range(len(list)):
+       g = suchen_grosst(list)
+       res.append(list.pop(g)) 
+       #pop() method uses index, n return the num just poped
+    return res
+
+print selection_sort([3, 5, 4, 1, 10])
