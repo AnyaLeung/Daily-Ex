cd31e8f7cedd2abde05a3e4687bde67bbbb9e18b
anyaliang <anyaliangww@gmail.com>
2017-08-15 15:35:56 +0800

    updated algorithm

---
+++ Algorithm/Sort/quick_sort.py
@@ -0,0 +1,13 @@
+def quick_sort(list):
+    if len(list) < 2:
+        return list #one element
+    else:
+        pivot = list[0]
+        klein = [i for i in list[1:] if i<= pivot]
+        gross = [i for i in list[1:] if i > pivot]
+
+        return quick_sort(klein) + [pivot] + quick_sort(gross)
+
+#base line condition: len(list/array) is empty OR one element
+
+print quick_sort([6, 5, 4, 3, 2, 1])
