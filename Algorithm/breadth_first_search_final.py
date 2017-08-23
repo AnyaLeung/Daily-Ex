2aca601bdb99d95402d6cb3c6b6ebe63913eb600
anyaliang <anyaliangww@gmail.com>
2017-08-23 09:14:05 +0800

    updated breadth_first_search

--- Algorithm/breadth_first_search_final.py
+++ Algorithm/breadth_first_search_final.py
@@ -6,21 +6,21 @@ def is_seller(name):
     return name[-1] == 'm' #last letter of name is, then true
 
 def breadth_first_search(graph, start):
-    search_queue = deque() #create a *double-end* deque (...?)
-    search_queue += graph["you"] #add all ur neighbors into queue
+    search_queue = deque() 
+    search_queue += graph[start] 
 
-    searched = {}
-    while search_queue: #not empty
+    searched = {} #查重的字典
+    count = 0
+    while search_queue:
         person = search_queue.popleft() 
-        if not searched[person]: 
-            #if haven't searched, add into the dict and check
+
+        #查重复
+        if not searched.get(person): 
             searched[person] = True
+        #查重复
+
             if is_seller(person): #符合条件
                 return True
             else: #否则把它的邻居都加进去
                 search_queue += graph[person]
-    return False #关系网络里没有
-
-breadth_first_search(graph, "you")
-
-
+    return False 
