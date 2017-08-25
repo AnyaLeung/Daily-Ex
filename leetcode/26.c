4dccc98819df6168fe1bf03dfb64e5a0eb202ea6
anyaliang <anyaliangww@gmail.com>
2017-08-25 10:22:15 +0800

    Add leetcode 26

--- leetcode/26.c
+++ leetcode/26.c
@@ -1,30 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 /*
- * Given a sorted array,
- * remove the duplicates in place
- * such that each element appear only once
- * return the new length.
+ * Given a sorted array, 
+ * remove the duplicates in place 
+ * such that each element appear only once.
  *
+ * Return the new length.
  */
 
 int removeDuplicates(int* nums, int numsSize) {
-    if(numsSize==0) return;
+    if(!numsSize) return 0;
 
     int count = 1;
-    if(numsSize==0) return 
-    for(int i=0; i<numsSize-1; i++){
-        if(nums[i]!=nums[i+1]){
+    for(int i=1; i<numsSize; i++){
+        if(!(nums[i]==nums[count-1])){
+            nums[count] = nums[i];
             count++;
         }
     }
     return count;
-} //no need to judge the first, must be unique
-
-int main(void){
-    int arr[0] = {1, 1, 1, 4, 5, 5, 6, 8, 9, 10};
-    int res = 0;
-    res = removeDuplicates(arr, 10);
-    printf("result: %d", res);
-    return 0;
 }
+
