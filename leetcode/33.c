fb6c791255d10f92f25d2b145918b09dcc9c50b0
anyaliang <anyaliangww@gmail.com>
2017-12-13 15:30:23 +0800

    Add 5.cpp leetcode

--- leetcode/33.c
+++
@@ -1,7 +0,0 @@
-int search(int* nums, int numsSize, int target) {
-    if(numsSize==0) return -1;
-    for(int i=0; i<numsSize; i++) {
-        if(nums[i]==target) return i;
-    }
-    return -1;
-}
