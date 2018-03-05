d0c8402d35a486093d4e16bc0415088dc52ea824
anyaliang <anyaliangww@gmail.com>
2018-03-05 17:54:20 +0800

    Create and update Java_learning

---
+++ Java_learning/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+    public static int gvar;
+    public static void say(String s) {
+        int x = 10;
+        System.out.print(s+x);
+    }
+    public static void main(String[] argv) {
+        float y = 0;
+        say("Hello, world\n");
+    }
+}
