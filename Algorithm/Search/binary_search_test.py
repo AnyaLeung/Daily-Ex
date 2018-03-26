35d28e03a90ca5ce57e5cd06dd550cc1e265063e
anyaliang <anyaliangww@gmail.com>
2018-03-26 13:23:57 +0800

    Update

--- Algorithm/Search/binary_search_test.py
+++
@@ -1,8 +0,0 @@
-from binary_search import binary_search
-#from binary_search import binary_search
-list = [1, 3, 5, 7, 21, 19]
-des =21 
-
-no = binary_search(list, des)
-print no
-
--- Algorithm/Search/selection_sort.py
+++
@@ -1,9 +0,0 @@
-def bubble_sort(list):
-    res = []
-    while len(list) != 0:
-        for item in range(len(list)): #range(len(list))
-            min_index = 0
-            if list[item] < list[min_index]:
-                 min_index = item
-        res.append(list.pop(min_index))
-    return res
