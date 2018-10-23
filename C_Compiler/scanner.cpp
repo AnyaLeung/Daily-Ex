9683f2e35f83fd6e2e7da9a21c2664eda656928e
anyaliang <anyaliangww@gmail.com>
2018-10-23 20:24:19 +0800

    Update scanner.cpp

--- C_Compiler/scanner.cpp
+++ C_Compiler/scanner.cpp
@@ -3,123 +3,54 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <vector>
-//keywords: begin, if, then, while, do, end
-//oprators: = + - * / < <= <> > >= = ;
+
 using namespace std;
 
-/* global var */
-char input[255] = "";
-int token_no = 0; //amount o' tokens
-char token_text[255];
-int p_index; //present char index
+/* global variables */
+char input[100] = "";
+int input_count = 0;
 
-//dict
+char s_word[10] = "";
+int s_count = 0;
 
-/* global def */
+int p_index = 0;
+
+
+/* global variables end */
+
 class WORD{
     public:
         int typenum;
         string word_name;
 };
 
-/* single char process */
+
 void WsIgnorance(){
-    while(input[p_index]==' '|| input[p_index]==10)
-        p_index++;
-} //ok
-
-bool IsLetter(){
-    if(input[p_index]>='a'&&input[p_index]<='z'){
-        return true;
+    while(input[input_count]==' '|| input[input_count]==10){
+        input_count++;
     }
-
-    if(input[p_index]>='A'&&input[p_index]<='Z'){
-        return true;
-    }
-    return false;
-} //ok
-
-bool IsDigit(){
-    if(input[p_index]>='0' && input[p_index]<='9'){
-        return true;
-    }
-
-    return false;
 }
 
-bool IsEnding(){
-    if(input[p_index]=='\0'){
-        return true;
-    }
-    return false;
+bool IsDelimiter(){
+    return (input[input_count]==' ' || input[input_count]=='\0');
 }
 
-bool IsID(){
-    char word[255];
-    char *p = word;
-    int count = 0;
-
-    if(IsLetter()){
-        word[count++] = input[p_index];
-        p_index++;
-        
-        while(!IsEnding()){
-            if(IsLetter() || IsDigit()){
-                word[count++] = input[p_index];
-                p_index++;
-            }
-            else{
-                return false;
-            }
+void SeperatePauseInput(void){
+    if(IsDelimiter() || input_count==0){
+        int n = 0;
+        WsIgnorance();
+        while(!IsDelimiter()){
+            s_word[s_count++] = input[input_count++];
         }
     }
-    else{
-        return false;
-    }
-
-    //cout << "word: " << word;
-    strcmp(token_text, word); return true;
 }
 
-/*
-   bool IsID(){
-   char word[255];
-    char *p
-    int count = 0;
-    bool flag = false;
-    
-    if(IsLetter()){
-        word[count++] = input[p_index-1]; //save first char
-        flag = true;
-        while(!IsEnding() && flag){
-            if(IsLetter() || IsDigit())
-                word[count++] = input[p_index-1]; //save other words
-            else flag = false; 
-        }
-    }
-    else return false;
-
-    if(flag){
-        strcpy(token_text, word);
-        cout << "token_text: " << token_text;
-        return true;
-    }
-    else return false;
-}
-*/
-/* single char process end */
-
-void Scanner(){
-    WsIgnorance();
-    IsID();
-}
-
-void PrintRes(WORD w){
-    cout << "(" << w.typenum << ", " << w.word_name << ")";
-}
 
 int main(void){
     cout << "Enter your program(end with #):";
     scanf("%[^#]s", input);
-    Scanner();
+
+    SeperatePauseInput();
+    
+    return 0;
 }
