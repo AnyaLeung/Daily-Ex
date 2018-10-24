5c595e4c49196ba6fdb62c7a3470f91a333c2d0c
anyaliang <anyaliangww@gmail.com>
2018-10-24 18:42:07 +0800

    Update scanner.cpp

--- C_Compiler/scanner.cpp
+++ C_Compiler/scanner.cpp
@@ -10,7 +10,7 @@ using namespace std;
 char input[100] = "";
 int input_count = 0;
 
-char s_word[10] = "";
+char s_word[100] = "";
 int s_count = 0;
 
 int p_index = 0;
@@ -31,26 +31,49 @@ void WsIgnorance(){
     }
 }
 
+bool IsLetter(){
+    if(input[p_index]>='a'&&input[p_index]<='z'){
+        return true;
+    }
+
+    if(input[p_index]>='A'&&input[p_index]<='Z'){
+        return true;
+    }
+    return false;
+}
+
+bool IsDigit(){
+    return (input[p_index]>='0' && input[p_index]<='9');
+}
+
 bool IsDelimiter(){
     return (input[input_count]==' ' || input[input_count]=='\0');
 }
 
+void BackSpace(int n){
+    p_index -= n;
+}
+
 void SeperatePauseInput(void){
-    if(IsDelimiter() || input_count==0){
-        int n = 0;
         WsIgnorance();
         while(!IsDelimiter()){
             s_word[s_count++] = input[input_count++];
+            cout << "s_count: " << s_count << endl;
         }
-    }
+        s_word[s_count++] = '#';
+        WsIgnorance();
 }
-
 
 int main(void){
     cout << "Enter your program(end with #):";
     scanf("%[^#]s", input);
+    cout << "input: " << input << endl;
 
-    SeperatePauseInput();
+    while(input[input_count]!='\0'){
+        SeperatePauseInput(); //divide into words
+        cout << "s_word: " << s_word << endl;
+        cout << endl;
+    }
     
     return 0;
 }
