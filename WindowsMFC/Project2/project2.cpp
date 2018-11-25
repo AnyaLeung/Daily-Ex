bdf99b2c223b5f0545fb819e287bd5de19bf15ec
anyaliang <anyaliangww@gmail.com>
2018-11-25 15:44:05 +0800

    Complete Project2

--- WindowsMFC/Project2/project2.cpp
+++ WindowsMFC/Project2/project2.cpp
@@ -1,14 +1,16 @@
-// WindowsProject8.cpp : 定义应用程序的入口点。
-//
-
 #include "stdafx.h"
-#include "WindowsProject8.h"
+#include "WindowsProject6.h"
 #include <tchar.h>
 #include <Windows.h>
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
+
 #define MAX_LOADSTRING 100
+
+static short lineCount = 0;
+short wordCount = 0;
+int x = 0, y = 0;
 
 // 全局变量:
 HINSTANCE hInst;                                // 当前实例
@@ -20,47 +22,45 @@ ATOM                MyRegisterClass(HINSTANCE hInstance);
 BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
-HFONT CreateMyFont(HDC hdc, int height, LPCWSTR FontName, COLORREF FontColor);
+HFONT CreateMyFont(HDC hDC, int height, LPCWSTR , COLORREF color);
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR    lpCmdLine,
+	_In_ int       nCmdShow)
 {
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
 
-    // TODO: 在此处放置代码。
+	// TODO: 在此处放置代码。
 
-    // 初始化全局字符串
-    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_WINDOWSPROJECT8, szWindowClass, MAX_LOADSTRING);
-    MyRegisterClass(hInstance);
+	// 初始化全局字符串
+	LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadStringW(hInstance, IDC_WINDOWSPROJECT6, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
 
-    // 执行应用程序初始化:
-    if (!InitInstance (hInstance, nCmdShow))
-    {
-        return FALSE;
-    }
+	// 执行应用程序初始化:
+	if (!InitInstance(hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
 
-    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_WINDOWSPROJECT8));
+	HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_WINDOWSPROJECT6));
 
-    MSG msg;
+	MSG msg;
 
-    // 主消息循环:
-    while (GetMessage(&msg, nullptr, 0, 0))
-    {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-    }
+	// 主消息循环:
+	while (GetMessage(&msg, nullptr, 0, 0))
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
 
-    return (int) msg.wParam;
+	return (int)msg.wParam;
 }
-
-
 
 //
 //  函数: MyRegisterClass()
@@ -69,23 +69,23 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 //
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
-    WNDCLASSEXW wcex;
+	WNDCLASSEXW wcex;
 
-    wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.cbSize = sizeof(WNDCLASSEX);
 
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WINDOWSPROJECT8));
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_WINDOWSPROJECT8);
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WINDOWSPROJECT6));
+	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_WINDOWSPROJECT6);
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
-    return RegisterClassExW(&wcex);
+	return RegisterClassExW(&wcex);
 }
 
 //
@@ -100,21 +100,23 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 //
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
-   hInst = hInstance; // 将实例句柄存储在全局变量中
+	hInst = hInstance; // 将实例句柄存储在全局变量中
 
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+	HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
 
-   if (!hWnd)
-   {
-      return FALSE;
-   }
+	if (!hWnd)
+	{
+		return FALSE;
+	}
 
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
 
-   return TRUE;
+	return TRUE;
 }
+
+
 
 //
 //  函数: WndProc(HWND, UINT, WPARAM, LPARAM)
@@ -132,172 +134,105 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	HFONT hf;
 	PAINTSTRUCT PtStr;
 	TEXTMETRIC tm;
-	POINT pos[5][100]; //record every word position
-	int i = 0;
-	static short lineCount = 0;
-	short wordCount = 0;
+	
 	LPCWSTR psz[] = {
-		L"This is the first line",
-		L"This is the second line",
-		L"This is the third line",
-		L"This is the fourth line",
-		L"This is the fifth line"
+		L"这是测试样例的第一行（楷体）",
+		L"这是测试样例的第二行（黑体）",
+		L"这是测试样例的第三行（宋体）",
+		L"这是测试样例的第四行（黑体）",
+		L"这是测试样例的第五行（楷体）"
 	};
 	LPCWSTR fontsName[] = {
 		L"楷体",
 		L"黑体",
-		L"隶书",
+		L"宋体",
 		L"黑体",
 		L"楷体"
 	};
 
-	//cal length o' every line
+	//calculate length o' every line
 	int lineSize[5];
-	for (int i=0; i<5; i++) {
+	for (int i = 0; i < 5; i++) {
 		lineSize[i] = lstrlen(psz[i]);
 	}
 
-    switch (message)
-    {
-    case WM_COMMAND:
-        {
-            int wmId = LOWORD(wParam);
-            // 分析菜单选择:
-            switch (wmId)
-            {
-            case IDM_ABOUT:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-                break;
-            case IDM_EXIT:
-                DestroyWindow(hWnd);
-                break;
-            default:
-                return DefWindowProc(hWnd, message, wParam, lParam);
-            }
-        }
-        break;
-	case WM_CREATE:
-		while (!SetTimer(hWnd, 1, 2000, NULL))
-				PostQuitMessage(1);
+	switch (message)
+	{
+	case WM_CREATE:	
+		SetTimer(hWnd, 1, 2000, NULL);
 		break;
-    case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
+	case WM_PAINT:											
+		hdc = BeginPaint(hWnd, &PtStr);						
+		wordCount = 0;
 
-			Sleep(800);
+		Sleep(800);
 
-			while (wordCount <= lineSize[lineCount]) {
-				printf("linecount:");
-				printf("%d", lineCount);
-				printf("    ");
-				printf("wordcount:");
-				printf("%d", wordCount);
-				printf("    ");
+		//calculate y position, changes every line
+		y = lineCount ? (y + 40) : 10;
 
-				//calculate position
-				if (!wordCount) {
-					pos[lineCount][wordCount].x = 0;
-				}
-				else {
-					pos[lineCount][wordCount].x = pos[lineCount][wordCount - 1].x + pos[lineCount][wordCount].x;
-				}
+		while (wordCount < lineSize[lineCount]) {
+			//calculate x position, changes every word
+			x = wordCount ? (x + 40) : 10;
 
-				if (!lineCount) {
-					pos[lineCount][wordCount].y = 0;
-				}
-				else {
-					pos[lineCount][wordCount].y = pos[lineCount][wordCount].y + pos[lineCount - 1][wordCount].y;
-				}
+			//calculate font size, from 8 to 40
+			double tmp = (double)32 / (double)lineSize[lineCount];
+			double height = (double)8 + tmp * (double)wordCount;
+			int height_para = (int)height;
 
-				//calculate font size, from 8 to 40
-				float tmp = 32 / lineSize[lineCount];
-				int height = 8 + (int)tmp * wordCount;
-				printf("height:");
-				printf("%d", height);
-				printf("     ");
+			//calculate font colour, from 0, 0, 0, to 255, 255, 255
+			tmp = 255 / (double)lineSize[lineCount];
+			COLORREF cr = RGB(wordCount*tmp, wordCount*tmp, wordCount*tmp);
 
-				//calculate font color, from RGB(0, 0, 0) to RGB(255, 255, 255)
-				tmp = 255 / lineSize[lineCount];
-				printf("rgb:");
-				printf("%f", tmp);
-				COLORREF cr = RGB(wordCount*tmp, wordCount*tmp, wordCount*tmp);
+			//set hf
+			hf = CreateMyFont(hdc, height_para, fontsName[lineCount], cr);
+			SelectObject(hdc, hf);
 
-				//print current char
-				hf = CreateMyFont(hdc, height, fontsName[lineCount], cr);
-				WCHAR word[1];
-				word[0] = psz[lineCount][wordCount];
-				LPCWSTR pszWordAfterConversion = word;
-				TextOut(hdc, pos[lineCount][wordCount].x, pos[lineCount][wordCount].y,pszWordAfterConversion, 1);
+			//get current char and print
+			WCHAR word[1];
+			word[0] = psz[lineCount][wordCount];
+			LPCWSTR pszWordAfterConversion = word;
+			TextOut(hdc, x, y, pszWordAfterConversion, 1);
 
-
-				wordCount++;
-			}
-			
-            EndPaint(hWnd, &ps);
-        }
-        break;
-	case WM_TIMER:
+			wordCount++;
+		}
+		break;
+	case WM_TIMER:	
 		if (wParam == 1)
 		{
-			lineCount = lineCount + 1;
-			if (lineCount == 5) {
-				lineCount = 0;
-			}
+			lineCount = (lineCount + 1) % 5;
 			InvalidateRect(hWnd, NULL, TRUE);
 		}
 		break;
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-    default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
-    }
-    return 0;
+	case WM_DESTROY:								
+		PostQuitMessage(0);									
+		break;
+	default:												
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
 }
 
-// “关于”框的消息处理程序。
-INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+HFONT CreateMyFont(HDC hDC, int height, LPCWSTR FontName, COLORREF CR)
 {
-    UNREFERENCED_PARAMETER(lParam);
-    switch (message)
-    {
-    case WM_INITDIALOG:
-        return (INT_PTR)TRUE;
+	HFONT hF;	
 
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-        {
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)TRUE;
-        }
-        break;
-    }
-    return (INT_PTR)FALSE;
-}
-
-HFONT CreateMyFont(HDC hdc, int height, LPCWSTR FontName, COLORREF FontColor)
-{
-	HFONT hf;
-
-	hf = CreateFont(
-		height,
-		0,
-		0,
-		0,
-		FW_NORMAL,
-		0,
-		0,
-		0,
-		GB2312_CHARSET,
-		OUT_DEFAULT_PRECIS,
+	hF = CreateFont(
+		height,			
+		0,						
+		0,						
+		0,						
+		FW_NORMAL,				
+		0,						
+		0,						
+		0,						
+		GB2312_CHARSET,			
+		OUT_DEFAULT_PRECIS,		
 		CLIP_DEFAULT_PRECIS,
-		DEFAULT_QUALITY,
-		DEFAULT_PITCH,
-		FontName
+		DEFAULT_QUALITY,		
+		DEFAULT_PITCH,			
+		FontName			
 	);
 
-	SetTextColor(hdc, FontColor);
-	return hf;
+	SetTextColor(hDC, CR);	//设置字体颜色
+	return hF;
 }
-
